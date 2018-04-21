6e41bfd861e5af06203e744148f1a76c3490469c
Efe <cj.dmob@gmail.com>
2018-04-21 18:39:26 +0100

    Fix uninitialized firebase app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,19 @@
-import {DocumentReference} from '@google-cloud/firestore';
-import * as admin from 'firebase-admin';
-import * as functions from 'firebase-functions';
+import admin = require("firebase-admin");
+import {firestore} from 'firebase-functions';
 
-export const countComics = functions.firestore.document('comics/{page}').onWrite((delta, context) => {
+admin.initializeApp();
 
-  const countRef: DocumentReference = admin.firestore().collection('internal').doc('comics-meta');
+export const countComics = firestore.document('comics/{page}').onWrite((delta, context) => {
+
+  const comicsMeta = admin.firestore().collection('internal').doc('comics-meta');
 
   if (delta.before.exists && delta.after.exists) return Promise.resolve(); // Update, do nothing
 
-  if (!delta.before.exists) return countRef.get() // Create
-    .then(snapshot => countRef.set({'total-count': (snapshot.get('total-count') || 0) + 1}));
+  if (!delta.before.exists) return comicsMeta.get() // Create
+    .then(snapshot => comicsMeta.set({'total-count': (snapshot.get('total-count') || 0) + 1}));
 
-  if (!delta.after.exists) return countRef.get() // Delete
-    .then(snapshot => countRef.set({'total-count': (snapshot.get('total-count') || 0) - 1}));
+  if (!delta.after.exists) return comicsMeta.get() // Delete
+    .then(snapshot => comicsMeta.set({'total-count': (snapshot.get('total-count') || 0) - 1}));
 
   return Promise.resolve();
 });

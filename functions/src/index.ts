6364d9d3d221dd5377f5fe8a185cbb40bfaf1ff5
Efe <cj.dmob@gmail.com>
2018-04-21 21:05:46 +0100

    Switch to using transactions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,41 +2,36 @@ import {firestore} from 'firebase-functions';
 import admin = require("firebase-admin");
 import winston = require("winston");
 
+const Logger = winston.Logger;
+const Console = winston.transports.Console;
+
+const {LoggingWinston} = require('@google-cloud/logging-winston');
+
+const logger = new Logger({
+  level: 'info',
+  transports: [new Console(), new LoggingWinston()],
+});
+
 admin.initializeApp();
 
-export const countComics = firestore.document('comics/{page}').onWrite(async (delta, context) => {
+export const incComics = firestore.document('comics/{page}').onCreate(async (snapshot, context) => {
 
-  winston.info(`${context.authType} calls ${context.eventType} on ${context.resource}`, {params: context.params});
+  const metaRef = admin.firestore().collection('internal').doc('comics-meta');
 
-  const page = context.params.page;
-  const internal = admin.firestore().collection('internal');
+  await admin.firestore().runTransaction(async transaction => {
+    const count = (await transaction.get(metaRef)).get('total-count') || 0;
+    await transaction.set(metaRef, {'total-count': count + 1}, {merge: true});
+  });
 
-  winston.info(``, {dataBeforeWrite: delta.before.exists, dataAfterWrite: delta.after.exists});
+});
 
-  // Updating item in place, exit early
-  if (delta.before.exists && delta.after.exists) {
-    winston.info(`Updating comic`, {page});
-    winston.info(`Do nothing`);
-    return;
-  }
+export const decComics = firestore.document('comics/{page}').onDelete(async (snapshot, context) => {
 
-  const count: number = (await internal.doc('comics-meta').get()).get('total-count') || 0;
+  const metaRef = admin.firestore().collection('internal').doc('comics-meta');
 
-  if (!delta.before.exists) { // Create
-    winston.info(`Creating comic`, {page});
-    winston.info(`Previous total count = ${count}`);
-    winston.info(`Try increment total count`);
-    await internal.doc('comics-meta').set({'total-count': count + 1}, {merge: true});
-    winston.info(`Increment total count success`);
-    return;
-  }
+  await admin.firestore().runTransaction(async transaction => {
+    const count = (await transaction.get(metaRef)).get('total-count') || 0;
+    await transaction.set(metaRef, {'total-count': count - 1}, {merge: true});
+  });
 
-  if (!delta.after.exists) { // Delete
-    winston.info(`Deleting comic`, {page});
-    winston.info(`Previous total count = ${count}`);
-    winston.info(`Try decrement total count`);
-    await internal.doc('comics-meta').set({'total-count': count - 1}, {merge: true});
-    winston.info(`Decrement total count success`);
-    return;
-  }
 });
